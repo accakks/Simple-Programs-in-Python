1353befcb5ce664e1ff8b16af61731eb9e81af8f
Akashkpdroid <71496133+Akashkpdroid@users.noreply.github.com>
2020-10-01 08:49:39 +0530

    digit extraction and adding take place in the same line, improving time for execution

--- powerDigitSum.py
+++ powerDigitSum.py
@@ -2,17 +2,13 @@
 # 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
 num = 2 ** 15 # 2^15
-mod = 0
 sum = 0
 
-while num != 0:
-  # gets the last digit
-  mod = num%10 
-  
-  # adds up
-  sum += mod
+while num != 0: 
+  # adds up while taking the last digit simultaneously
+  sum += (num%10)
   
   # removes the last digit
-  num = num/10
+  num /=10
   
 print(sum)
